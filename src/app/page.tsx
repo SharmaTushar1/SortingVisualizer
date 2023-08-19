ce819390ca53357c829167cc3b789bc31de28b96
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-08-19 18:36:54 +0530

    Add home main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex justify-center items-center flex-col">
       <h1 className="text-6xl">Sorting Visualizer</h1>
       <div>- Tushar Sharma</div>
-      <button>
+      <Link href={'/home'}>
         Let&#39;s Sort!!
-      </button>
+      </Link>
     </main>
   )
 }
