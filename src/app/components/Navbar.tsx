1b6ff3450d45d6c5f5f162aaaadd911820f02021
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-08-22 17:47:49 +0530

    Add source code link and minor ui changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import ThemeToggleIcon from "./ThemeToggleIcon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
     <div className="flex justify-between mt-4">
       <Link href={'/'} className="text-2xl">Sorting Visualizer</Link>
-      <ThemeToggleIcon />
+      <div className="flex w-[58px] items-center justify-between">
+        <ThemeToggleIcon />
+        <Link href={"https://github.com/SharmaTushar1/SortingVisualizer"}>
+          <FontAwesomeIcon icon={faCode} className="fa-lg" />
+        </Link>
+      </div>
     </div>
   )
 }
--- src/app/components/ThemeToggleIcon.tsx
+++ src/app/components/ThemeToggleIcon.tsx
@@ -9,10 +9,10 @@ export default function ThemeToggleIcon() {
   const { theme, setTheme } = useTheme();
   const [isDarkMode, setIsDarkMode] = useState(theme==='dark' || false);
   return (
-    <div className='cursor-pointer mr-4 mt-1 sm:mr-0 sm:mt-0 z-10'>
+    <div className='cursor-pointer z-10'>
       {isDarkMode
-        ? <FontAwesomeIcon className='w-6 h-6 sm:w-6 sm:h-6' icon={faSun} onClick={() => {setTheme("light"); setIsDarkMode(prev => !prev);}}/>
-        : <FontAwesomeIcon className='w-6 h-6 sm:w-6 sm:h-6' icon={faMoon} onClick={() => {setTheme("dark"); setIsDarkMode(prev => !prev);}} />
+        ? <FontAwesomeIcon className='fa-lg' icon={faSun} onClick={() => {setTheme("light"); setIsDarkMode(prev => !prev);}}/>
+        : <FontAwesomeIcon className='fa-lg' icon={faMoon} onClick={() => {setTheme("dark"); setIsDarkMode(prev => !prev);}} />
       }
     </div>
   );
