2511781edebcaa12b542a548b3c5ea1e2add85c5
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-08-22 20:22:31 +0530

    Add status to toggle status

--- src/app/home/components/ToggleStats.tsx
+++ src/app/home/components/ToggleStats.tsx
@@ -2,9 +2,39 @@
 
 import {useState} from 'react';
 
+type stats = {
+  algorithm: string,
+  timeTaken: string,
+  comparisonsMade: string,
+  isStable: boolean,
+  memoryUsage: string,
+  arraySize: number,
+}
+
 export default function ToggleStats() {
 
   const [visible, setVisible] = useState(false);
+
+  const [algorithm, setAlgorithm] = useState("");
+
+  const [timeTaken, setTimeTaken] = useState("");
+
+  const [comparisonsMade, setComparisonsMade] = useState("");
+
+  const [isStable, setIsStable] = useState(false);
+
+  const [memoryUsage, setMemoryUsage] = useState("");
+
+  const [arraySize, setArraySize] = useState(0);
+
+  const props:stats = {
+    algorithm,
+    timeTaken,
+    comparisonsMade,
+    isStable,
+    memoryUsage,
+    arraySize,
+  }
 
   const toggleStats = () => {
     console.log(visible);
@@ -14,20 +44,51 @@ export default function ToggleStats() {
 
   return (
     <div className='relative'>
-      <div className='absolute right-0'>
-        <button className="bg-zinc-200 dark:bg-zinc-800 text-sm p-2 border-none rounded-xl" onClick={() => toggleStats()}>
+      <div className='absolute flex flex-col right-0 z-10 items-end'>
+        <button className="bg-zinc-200 dark:bg-zinc-800 text-sm p-2 border-none rounded-xl w-fit" onClick={() => toggleStats()}>
           {visible ? "Hide Stats": "Show Stats"}
         </button>
-        {visible ? <Stats />: ""}
+        <div className='mt-4'>
+          {visible ? <Stats
+          {...props}
+          />: ""}
+        </div>
       </div>
     </div>
   )
 }
 
-export function Stats() {
+export function Stats({...props}: stats) {
   return (
-    <div className='dark:bg-zinc-700 bg-zinc-300'>
-      Stats
+    <div className='dark:bg-zinc-700 bg-zinc-300 p-4'>
+      <div>
+        {/* Sorting... or Shuffling...  */}
+        Status: {"Status"} 
+      </div>
+      <div>
+        Algorithm: {props.algorithm}
+      </div>
+      <div>
+        {/* Time take for sorting (0 initially) */}
+        Total Time Taken: {props.timeTaken}
+      </div>
+      <div>
+        {/* Comparisons made while sorting (0 initially) */}
+        Comparisons made: {props.comparisonsMade}
+      </div>
+      <div>
+        {/* True or false */}
+        Stable: {props.timeTaken}
+      </div>
+      <div>
+        {/* Memory Usage */}
+        Memory Usage: {props.memoryUsage}
+      </div>
+      <div>
+        {/* Array Size */}
+        Array Size:
+        <input type="range" min="1" max="100" value={props.arraySize}/>
+      </div>
     </div>
   );
 }
