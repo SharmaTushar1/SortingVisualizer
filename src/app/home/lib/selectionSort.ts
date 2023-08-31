2485fb4538a8e4cbc13800d1191171fb84c0941a
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-08-31 17:51:37 +0530

    Fix selection sort visualization

--- src/app/home/lib/selectionSort.ts
+++ src/app/home/lib/selectionSort.ts
@@ -3,8 +3,10 @@ export default function selectionSort(array: number[]) {
   for (let j = 0; j<array.length-1; j++) {
     let iMin = j;
     for (let i = j+1; i<array.length; i++) {
-      if (array[i] < array[iMin])
+      if (array[i] < array[iMin]) {
         iMin = i;
+        swaps.push([iMin, i])
+      }
     }
     if (iMin != j) {
       swaps.push([j, iMin]);
